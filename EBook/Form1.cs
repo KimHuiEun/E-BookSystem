0cde902e6d4b1cdb3c47a327bad43615d33808d9
KEEHY <63379191+KEEHY@users.noreply.github.com>
2020-12-15 13:53:54 +0900

    스테이징

--- EBook/Form1.cs
+++ EBook/Form1.cs
@@ -8,13 +8,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace EBook
+namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Sday_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form1 form = new Form1();
+            
         }
     }
 }
