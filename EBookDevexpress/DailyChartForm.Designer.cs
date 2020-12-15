841b7a676d48f15dad825944233745cfb53689d1
KEEHY <63379191+KEEHY@users.noreply.github.com>
2020-12-15 15:16:59 +0900

    MainForm

--- EBookDevexpress/DailyChartForm.Designer.cs
+++ EBookDevexpress/DailyChartForm.Designer.cs
@@ -29,10 +29,17 @@ namespace EBookDevexpress
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // DailyChartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "DailyChartForm";
             this.Text = "DailyChart";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
