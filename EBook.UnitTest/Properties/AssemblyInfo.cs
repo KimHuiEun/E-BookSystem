a0e5a54180109009a591dc218e1db0c7d0154b2b
KEEHY <63379191+KEEHY@users.noreply.github.com>
2020-12-17 18:27:53 +0900

    단위테스트 프로젝트 추가. RentDao 단위테스트 클래스 추가.

---
+++ EBook.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EBook.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EBook.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("80b3bdec-de55-46ff-9a86-3d0c1cf68e93")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RentDaoTest/Dao/RentDaoTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EBook.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentDaoTest
+{
+    [TestClass()]
+    public class RentDaoTests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            Assert.Fail();
+        }
+
+        public void GetWithExtraTest()
+        {
+
+        }
+
+        public void GetCount()
+        {
+            int count = Dao.Rent.GetCount();
+
+            Assert.IsTrue(count > 0);
+        }
+
+
+    }
+}
