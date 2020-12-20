b5343f5991b7c09466e9d0a84eef32f62e5f186a
Chanyoung Park <chanmsworld@outlook.com>
2020-12-20 14:48:40 +0900

    수정해야 하는 사항 메모 해서 커밋하기.

--- EBookDevexpress/RankForm/NewBookRankForm.cs
+++ EBookDevexpress/RankForm/NewBookRankForm.cs
@@ -29,7 +29,10 @@ namespace EBookDevexpress
 
         private void genreSelectControl1_Load(object sender, EventArgs e)
         {
-           
+            //TODO : 모자른 소스 코드
+            //1. 클릭시에 따른 장르 가져오기
+            //2. 클릭을 해제시 처음 데이터 코드로 변경하기
+            //3. 책의 데여량 가져오는 쿼리문이 완성 됬던가...?
         }
 
         private void checkedListBoxControl1_ItemCheck(object sender, DevExpress.XtraEditors.Controls.ItemCheckEventArgs e)
@@ -44,12 +47,15 @@ namespace EBookDevexpress
                     s = s + "Item " + (i + 1).ToString() + " = " + checkedListBoxControl1.Items[i].ToString() + "\n";
                 }
             }
+            //버튼을 클릭시 메세지 박스 출력까지는 성공함.
+            //눌러진 버튼을 보내서 데이터베이스의 장르별 데이터에 접근하는 법 필요함.
             //MessageBox.Show(s);
         }
 
         private void NewBookRankForm_Load(object sender, EventArgs e)
         {
-            dbsNewbook.DataSource = Dao.Rent.BookCountRank(e.);
+            //TODO : 책 대여량 베스트 10 가져오는 소스 코드 필요함
+            //dbsNewbook.DataSource = Dao.Rent.BookCountRank(e.);
         }
     }
 }
